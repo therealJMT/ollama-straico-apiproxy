1406ebdc82cc23b0366b1078cfb55995ba131488
JRinaldi <jrinaldi@jayrinaldi.me>
2024-08-16 14:04:29 +0900

    set speech to text process to background  and non blocking

--- api_endpoints/lm_studio_transcription.py
+++ api_endpoints/lm_studio_transcription.py
@@ -6,12 +6,32 @@ from fastapi.responses import JSONResponse
 from app import app
 from typing import Optional
 import tempfile
-
+import asyncio
 from os import environ
 
 logger = logging.getLogger(__name__)
 
 default_transcription_model = environ.get("DEFAULT_TRANSCRIPTION_MODEL", "base")
+
+from multiprocessing import Process, Queue
+
+
+def process_transcribe(queue, filename, model_name):
+    model = whisper.load_model(model_name)
+    result = model.transcribe(filename)
+    queue.put(result["text"])
+
+
+async def process_in_background(queue, filename, model_name):
+    process = Process(target=process_transcribe, args=(queue, filename, model_name))
+    process.start()
+
+    while True:
+        if not queue.empty():
+            result = queue.get()
+            process.join()
+            return result
+        await asyncio.sleep(5)
 
 
 @app.post("/v1/audio/transcriptions")
@@ -20,10 +40,12 @@ async def lm_studio_transcriptions(
 ):
     contents = await file.read()
     model_name = default_transcription_model
-    model = whisper.load_model(model_name)
+
     with tempfile.TemporaryDirectory() as tmpdirname:
-        file = pathlib.Path(tmpdirname) / "audio.mp3"
+        file = pathlib.Path(tmpdirname) / file.filename
         with file.open("wb") as writer:
             writer.write(contents)
-        result = model.transcribe(str(file))
-        return JSONResponse(content={"text": result["text"]})
+
+        queue = Queue()
+        text = await process_in_background(queue, str(file), model_name)
+        return JSONResponse(content={"text": text})
